d153bb85a6783153d1896341383e17f5a01bf9a7
golf <bekaza759@gmail.com>
2015-02-04 09:27:09 +0700

    Complete Constraints

---
+++ lesson16/Homepwner/Homepwner/BNRDetailViewController.h
@@ -0,0 +1,19 @@
+//
+//  BNRDetailViewController.h
+//  Homepwner
+//
+//  Created by manit on 28/1/2558.
+//  Copyright (c) พ.ศ. 2558 golf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class BNRitem;
+
+@interface BNRDetailViewController : UIViewController
+
+@property (nonatomic, strong) BNRitem * item;
+
+
+
+@end
---
+++ lesson16/Homepwner/Homepwner/BNRItemsViewController.m
@@ -0,0 +1,154 @@
+//
+//  BNRItemsViewController.m
+//  Homepwner
+//
+//  Created by manit on 24/1/2558.
+//  Copyright (c) พ.ศ. 2558 golf. All rights reserved.
+//
+
+#import "BNRItemsViewController.h"
+#import "BNRItemStore.h"
+#import "BNRitem.h"
+#import "BNRDetailViewController.h"
+
+@interface BNRItemsViewController()
+
+//strong property beacuase a top-level object in the XIB file
+@property (nonatomic, strong)IBOutlet UIView * headerView;
+
+@end
+
+@implementation BNRItemsViewController
+
+- (instancetype)init
+{
+    // Call the superclass's designated initializer
+    self = [super initWithStyle:UITableViewStylePlain];
+    
+    if (self) {
+        UINavigationItem * navItem = self.navigationItem;
+        navItem.title = @"Homepwner";
+        
+        // Create a new bar button item that will send
+        // addNewItem: to BNRItemsViewController
+        UIBarButtonItem * bbi = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(addNewItem:)];
+        
+        // Set this bar button item as the right item in the navigationItem
+        navItem.rightBarButtonItem = bbi;
+        
+        navItem.leftBarButtonItem = self.editButtonItem;
+    }
+    
+    return self;
+}
+
+- (instancetype)initWithStyle:(UITableViewStyle)style
+{
+    return [self init];
+}
+
+-(void)tableView:(UITableView *)tableView
+didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    BNRDetailViewController * detailViewController = [[BNRDetailViewController alloc] init];
+    
+    NSArray * items = [[BNRItemStore sharedStore] allItems];
+    BNRitem * selectedItem = items[indexPath.row];
+    
+    //Give Detail view Controller a pointer to the item object in row
+    detailViewController.item = selectedItem;
+    
+    //Push it onto the top of the navigation controller's stack
+    [self.navigationController pushViewController:detailViewController
+                                         animated:YES];
+}
+
+- (NSInteger)tableView:(UITableView *)tableView
+            numberOfRowsInSection:(NSInteger)section
+{
+    return [[[BNRItemStore sharedStore] allItems] count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView
+         cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Get a new or recycled cell
+    UITableViewCell *cell =
+    [tableView dequeueReusableCellWithIdentifier:@"UITableViewCell"
+                                    forIndexPath:indexPath];
+    NSArray * items = [[BNRItemStore sharedStore] allItems];
+    BNRitem * item = items[indexPath.row];
+    cell.textLabel.text = [item description];
+    return cell;
+
+}
+
+-(void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    
+    [self.tableView reloadData];
+}
+
+-(void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    UIView * header = self.headerView;
+    [self.tableView setTableHeaderView:header];
+    
+    [self.tableView registerClass:[UITableViewCell class]
+           forCellReuseIdentifier:@"UITableViewCell"];
+    
+}
+
+//other method Here
+
+-(IBAction)addNewItem:(id)sender
+{
+    //Create a new BNRitem and add it to the store
+    BNRitem * newItem = [[BNRItemStore sharedStore] createItem];
+    
+    //Figure out where that item is in the array
+    NSInteger lastRow = [[[BNRItemStore sharedStore] allItems] indexOfObject:newItem];
+    
+    NSIndexPath * indexPath = [NSIndexPath indexPathForRow:lastRow inSection:0];
+    
+    //Insert this new row into the table.
+    [self.tableView insertRowsAtIndexPaths:@[indexPath]
+                          withRowAnimation:UITableViewRowAnimationTop];
+}
+
+-(void)tableView:(UITableView *)tableView
+commitEditingStyle:(UITableViewCellEditingStyle)editingStyle
+forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    //If the table view is asking to commit a delete command...
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        NSArray * items = [[BNRItemStore sharedStore] allItems];
+        BNRitem * item = items[indexPath.row];
+        [[BNRItemStore sharedStore] removeItem:item];
+        
+        //Also remove that row from the table view with an animation
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    }
+}
+
+-(void)tableView:(UITableView *)tableView
+moveRowAtIndexPath:(NSIndexPath *)sourceIndexPath
+     toIndexPath:(NSIndexPath *)destinationIndexPath
+{
+    [[BNRItemStore sharedStore]
+     moveItemAtIndex:sourceIndexPath.row
+     toIndex:destinationIndexPath.row];
+}
+
+
+-(NSString *)tableView:(UITableView *)tableView
+titleForDeleteConfirmationButtonForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return @"Remove";
+}
+
+
+@end
